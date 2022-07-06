56df5582726ece27555ae98308787c0aac7c4499
Alex Su <alexanderytsu@gmail.com>
2022-07-06 11:43:36 +1000

    author an actor that uses fil_token

--- testing/integration/actors/fil_token_actor/src/lib.rs
+++ testing/integration/actors/fil_token_actor/src/lib.rs
@@ -1,11 +1,23 @@
+use fil_token;
 use fvm_sdk as sdk;
 
 /// Placeholder invoke for testing
 #[no_mangle]
-pub fn invoke(_: u32) -> u32 {
+pub fn invoke(params: u32) -> u32 {
     // Conduct method dispatch. Handle input parameters and return data.
-    sdk::vm::abort(
-        fvm_shared::error::ExitCode::FIRST_USER_EXIT_CODE,
-        Some("sample abort"),
-    )
+    let method_num = sdk::message::method_number();
+
+    match method_num {
+        1 => constructor(),
+        _ => {
+            sdk::vm::abort(
+                fvm_shared::error::ExitCode::FIRST_USER_EXIT_CODE,
+                Some("sample abort"),
+            );
+        }
+    }
+}
+
+fn constructor() -> u32 {
+    0_u32
 }
