d89fe089423200202cd6e2f0b3d956e90a216633
Alex Su <alexanderytsu@gmail.com>
2022-07-12 17:36:30 +1000

    fvm_dispatch: handle hash collisions and invalid method names

--- fvm_dispatch/src/hash.rs
+++ fvm_dispatch/src/hash.rs
@@ -1,8 +1,11 @@
+use std::{error::Error, fmt::Display};
+
 use fvm_sdk::crypto;
 
 /// Minimal interface for a hashing function
 ///
-/// Hasher::hash() must return a digest that is at least 4 bytes long so that it can be cast to a u32
+/// Hasher::hash() must return a digest that is at least 4 bytes long so that it can be cast to a
+/// u32
 pub trait Hasher {
     /// For an input of bytes return a digest that is at least 4 bytes long
     fn hash(&self, bytes: &[u8]) -> Vec<u8>;
@@ -18,39 +21,84 @@ impl Hasher for Blake2bSyscall {
     }
 }
 
-/// Uses an underlying hashing function (blake2b by convention) to generate method numbers from method names
+/// Uses an underlying hashing function (blake2b by convention) to generate method numbers from
+/// method names
 #[derive(Default)]
 pub struct MethodResolver<T: Hasher> {
     hasher: T,
 }
 
+#[derive(PartialEq, Debug, Clone)]
+pub enum MethodNameErr {
+    EmptyString,
+    IllegalSymbol,
+    IndeterminableId,
+}
+
+impl Display for MethodNameErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MethodNameErr::IllegalSymbol => write!(f, "Illegal symbol used in method name"),
+            MethodNameErr::IndeterminableId => write!(
+                f,
+                "Unable to calculate method id, choose another method name"
+            ),
+            MethodNameErr::EmptyString => write!(f, "Empty method name provided"),
+        }
+    }
+}
+
+impl Error for MethodNameErr {}
+
 impl<T: Hasher> MethodResolver<T> {
     const CONSTRUCTOR_METHOD_NAME: &'static str = "Constructor";
     const CONSTRUCTOR_METHOD_NUMBER: u64 = 1_u64;
+    const RESERVED_METHOD_NUMBER: u64 = 0_u64;
+
+    /// Creates a MethodResolver with an instance of a hasher (blake2b by convention)
     pub fn new(hasher: T) -> Self {
         Self { hasher }
     }
 
-    pub fn method_number(&self, method_name: &str) -> u64 {
-        if method_name == Self::CONSTRUCTOR_METHOD_NAME {
-            Self::CONSTRUCTOR_METHOD_NUMBER
-        } else {
-            self.propose_number(method_name)
+    /// Generates a standard FRC-XXX compliant method number
+    ///
+    /// The method number is calculated as the first four bytes of `hash(method-name)`.
+    /// The name `Constructor` is always hashed to 1 and other method names that hash to
+    /// 0 or 1 are avoided via rejection sampling.
+    ///
+    ///
+    pub fn method_number(&self, method_name: &str) -> Result<u64, MethodNameErr> {
+        // TODO: sanitise method_name before checking (or reject invalid whitespace)
+        if method_name.contains("|") {
+            return Err(MethodNameErr::IllegalSymbol);
         }
-    }
 
-    fn propose_number(&self, method_name: &str) -> u64 {
-        let digest = self.hasher.hash(method_name.as_bytes());
-        if digest.len() < 4 {
-            panic!("Invalid hasher used: digest must be at least 4 bytes long");
+        if method_name.len() == 0 {
+            return Err(MethodNameErr::EmptyString);
         }
-        as_u32(digest.as_slice()) as u64
+
+        if method_name == Self::CONSTRUCTOR_METHOD_NAME {
+            Ok(Self::CONSTRUCTOR_METHOD_NUMBER)
+        } else {
+            let mut digest = self.hasher.hash(method_name.as_bytes());
+            while digest.len() >= 4 {
+                let method_id = as_u32(digest.as_slice());
+                if method_id as u64 != Self::CONSTRUCTOR_METHOD_NUMBER
+                    && method_id as u64 != Self::RESERVED_METHOD_NUMBER
+                {
+                    return Ok(as_u32(digest.as_slice()) as u64);
+                } else {
+                    digest.remove(0);
+                }
+            }
+            Err(MethodNameErr::IndeterminableId)
+        }
     }
 }
 
 /// Takes a byte array and interprets it as a u32 number
 /// 
-/// Using big-endian order essentially "casts" the first four bytes to an int
+/// Using big-endian order interperets the first four bytes to an int
 #[rustfmt::skip]
 fn as_u32(bytes: &[u8]) -> u32 {
     (bytes[0] as u32)              + 
@@ -62,7 +110,7 @@ fn as_u32(bytes: &[u8]) -> u32 {
 #[cfg(test)]
 mod tests {
 
-    use super::{Hasher, MethodResolver};
+    use super::{Hasher, MethodNameErr, MethodResolver};
 
     #[derive(Clone, Copy)]
     struct FakeHasher {}
@@ -73,18 +121,56 @@ mod tests {
     }
 
     #[test]
-    fn constructor_method_number() {
+    fn constructor_is_1() {
         let method_hasher = MethodResolver::new(FakeHasher {});
-        assert_eq!(method_hasher.method_number("Constructor"), 1);
+        assert_eq!(method_hasher.method_number("Constructor").unwrap(), 1);
     }
 
     #[test]
-    fn normal_method_number() {
+    fn normal_method_is_hashed() {
         let fake_hasher = FakeHasher {};
         let method_hasher = MethodResolver::new(fake_hasher);
         assert_eq!(
-            method_hasher.method_number("NormalMethod"),
+            method_hasher.method_number("NormalMethod").unwrap(),
             super::as_u32(&fake_hasher.hash(b"NormalMethod")) as u64
         );
+
+        assert_eq!(
+            method_hasher.method_number("NormalMethod2").unwrap(),
+            super::as_u32(&fake_hasher.hash(b"NormalMethod2")) as u64
+        );
+    }
+
+    #[test]
+    fn disallows_invalid_method_names() {
+        let method_hasher = MethodResolver::new(FakeHasher {});
+        assert_eq!(
+            method_hasher.method_number("Invalid|Method").unwrap_err(),
+            MethodNameErr::IllegalSymbol
+        );
+        assert_eq!(
+            method_hasher.method_number("").unwrap_err(),
+            MethodNameErr::IllegalSymbol
+        );
+    }
+
+    #[test]
+    fn avoids_disallowed_method_numbers() {
+        let hasher = FakeHasher {};
+        let method_hasher = MethodResolver::new(hasher);
+
+        // This simulates a method name that would hash to 0
+        let contrived_0 = "\0\0\0\0MethodName";
+        let contrived_0_digest = hasher.hash(contrived_0.as_bytes());
+        assert_eq!(super::as_u32(&contrived_0_digest), 0);
+        // But the method number is not a collision
+        assert_ne!(method_hasher.method_number(contrived_0).unwrap(), 0);
+
+        // This simulates a method name that would hash to 1
+        let contrived_1 = "\x01\0\0\0MethodName";
+        let contrived_1_digest = hasher.hash(contrived_1.as_bytes());
+        assert_eq!(super::as_u32(&contrived_1_digest), 1);
+        // But the method number is not a collision
+        assert_ne!(method_hasher.method_number(contrived_1).unwrap(), 1);
     }
 }
--- fvm_dispatch/src/message.rs
+++ fvm_dispatch/src/message.rs
@@ -1,30 +1,67 @@
-use crate::hash::{Hasher, MethodResolver};
+use std::{error::Error, fmt::Display};
+
+use crate::hash::{Hasher, MethodNameErr, MethodResolver};
 
 use fvm_ipld_encoding::RawBytes;
-use fvm_sdk::{send, SyscallResult};
+use fvm_sdk::{send, sys::ErrorNumber};
 use fvm_shared::{address::Address, econ::TokenAmount, receipt::Receipt};
 
-/// Utility to invoke standard methods on actors by publishing an on-chain Message
+/// Utility to invoke standard methods on deployed actors
 #[derive(Default)]
 pub struct MethodMessenger<T: Hasher> {
     method_resolver: MethodResolver<T>,
 }
 
+#[derive(PartialEq, Debug, Clone)]
+pub enum MethodMessengerError {
+    MethodName(MethodNameErr),
+    Syscall(ErrorNumber),
+}
+
+impl Display for MethodMessengerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MethodMessengerError::Syscall(e) => write!(f, "Error when sending message: {:?}", e),
+            MethodMessengerError::MethodName(e) => {
+                write!(f, "Error calculating method name: {:?}", e)
+            }
+        }
+    }
+}
+
+impl Error for MethodMessengerError {}
+
+impl From<ErrorNumber> for MethodMessengerError {
+    fn from(e: ErrorNumber) -> Self {
+        Self::Syscall(e)
+    }
+}
+
+impl From<MethodNameErr> for MethodMessengerError {
+    fn from(e: MethodNameErr) -> Self {
+        Self::MethodName(e)
+    }
+}
+
 impl<T: Hasher> MethodMessenger<T> {
+    /// Creates a new method messenger using a specified hashing function (blake2b by default)
     pub fn new(hasher: T) -> Self {
         Self {
             method_resolver: MethodResolver::new(hasher),
         }
     }
 
+    /// Calls a method (by name) on a specified actor by constructing and publishing the underlying
+    /// on-chain Message
     pub fn call_method(
         &self,
         to: &Address,
         method: &str,
         params: RawBytes,
         value: TokenAmount,
-    ) -> SyscallResult<Receipt> {
-        let method = self.method_resolver.method_number(method);
-        send::send(to, method, params, value)
+    ) -> Result<Receipt, MethodMessengerError> {
+        let method = self.method_resolver.method_number(method)?;
+        let res = send::send(to, method, params, value)?;
+        Ok(res)
     }
 }
