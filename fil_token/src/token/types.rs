dc3eb694b65322b77ef96f2db05f3bdbbc05d063
Alex Su <alexanderytsu@gmail.com>
2022-07-12 11:42:09 +1000

    revert dependency overrides

--- fil_token/src/token/types.rs
+++ fil_token/src/token/types.rs
@@ -1,4 +1,4 @@
-use fvm_ipld_encoding::tuple::*;
+use fvm_ipld_encoding::tuple::{Deserialize_tuple, Serialize_tuple};
 use fvm_ipld_encoding::{Cbor, RawBytes};
 use fvm_shared::address::Address;
 use fvm_shared::bigint::bigint_ser;
