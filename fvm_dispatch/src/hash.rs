b3d0530461cfd1792e0255866529b22cedbb56cd
Alex Su <alexanderytsu@gmail.com>
2022-07-12 15:29:26 +1000

    Renaming and docstrings

--- fvm_dispatch/src/hash.rs
+++ fvm_dispatch/src/hash.rs
@@ -18,12 +18,13 @@ impl Hasher for Blake2bSyscall {
     }
 }
 
+/// Uses an underlying hashing function (blake2b by convention) to generate method numbers from method names
 #[derive(Default)]
-pub struct MethodHasher<T: Hasher> {
+pub struct MethodResolver<T: Hasher> {
     hasher: T,
 }
 
-impl<T: Hasher> MethodHasher<T> {
+impl<T: Hasher> MethodResolver<T> {
     const CONSTRUCTOR_METHOD_NAME: &'static str = "Constructor";
     const CONSTRUCTOR_METHOD_NUMBER: u64 = 1_u64;
     pub fn new(hasher: T) -> Self {
@@ -34,29 +35,34 @@ impl<T: Hasher> MethodHasher<T> {
         if method_name == Self::CONSTRUCTOR_METHOD_NAME {
             Self::CONSTRUCTOR_METHOD_NUMBER
         } else {
-            let digest = self.hasher.hash(method_name.as_bytes());
-            if digest.len() < 4 {
-                panic!("Invalid hasher used: digest must be at least 4 bytes long");
-            }
-            as_u32(digest.as_slice()) as u64
+            self.propose_number(method_name)
         }
+    }
+
+    fn propose_number(&self, method_name: &str) -> u64 {
+        let digest = self.hasher.hash(method_name.as_bytes());
+        if digest.len() < 4 {
+            panic!("Invalid hasher used: digest must be at least 4 bytes long");
+        }
+        as_u32(digest.as_slice()) as u64
     }
 }
 
 /// Takes a byte array and interprets it as a u32 number
-/// Assumes little-endian order
+/// 
+/// Using big-endian order essentially "casts" the first four bytes to an int
 #[rustfmt::skip]
 fn as_u32(bytes: &[u8]) -> u32 {
-    ((bytes[0] as u32) << (8 * 3)) +
-    ((bytes[1] as u32) << (8 * 2)) +
-    ((bytes[2] as u32) << (8 * 1)) +
-    (bytes[3] as u32)
+    (bytes[0] as u32)              + 
+    ((bytes[1] as u32) << (8 * 1)) +
+    ((bytes[2] as u32) << (8 * 2)) +
+    ((bytes[3] as u32) << (8 * 3)) 
 }
 
 #[cfg(test)]
 mod tests {
 
-    use super::{Blake2bSyscall, Hasher, MethodHasher};
+    use super::{Hasher, MethodResolver};
 
     #[derive(Clone, Copy)]
     struct FakeHasher {}
@@ -67,21 +73,15 @@ mod tests {
     }
 
     #[test]
-    #[allow(unused)]
-    fn compile() {
-        let method_hasher = MethodHasher::new(Blake2bSyscall {});
-    }
-
-    #[test]
     fn constructor_method_number() {
-        let method_hasher = MethodHasher::new(FakeHasher {});
+        let method_hasher = MethodResolver::new(FakeHasher {});
         assert_eq!(method_hasher.method_number("Constructor"), 1);
     }
 
     #[test]
     fn normal_method_number() {
         let fake_hasher = FakeHasher {};
-        let method_hasher = MethodHasher::new(fake_hasher);
+        let method_hasher = MethodResolver::new(fake_hasher);
         assert_eq!(
             method_hasher.method_number("NormalMethod"),
             super::as_u32(&fake_hasher.hash(b"NormalMethod")) as u64
--- fvm_dispatch/src/message.rs
+++ fvm_dispatch/src/message.rs
@@ -1,18 +1,19 @@
-use crate::hash::{Hasher, MethodHasher};
+use crate::hash::{Hasher, MethodResolver};
 
 use fvm_ipld_encoding::RawBytes;
 use fvm_sdk::{send, SyscallResult};
 use fvm_shared::{address::Address, econ::TokenAmount, receipt::Receipt};
 
+/// Utility to invoke standard methods on actors by publishing an on-chain Message
 #[derive(Default)]
-pub struct MethodDispatcher<T: Hasher> {
-    method_hasher: MethodHasher<T>,
+pub struct MethodMessenger<T: Hasher> {
+    method_resolver: MethodResolver<T>,
 }
 
-impl<T: Hasher> MethodDispatcher<T> {
+impl<T: Hasher> MethodMessenger<T> {
     pub fn new(hasher: T) -> Self {
         Self {
-            method_hasher: MethodHasher::new(hasher),
+            method_resolver: MethodResolver::new(hasher),
         }
     }
 
@@ -23,7 +24,7 @@ impl<T: Hasher> MethodDispatcher<T> {
         params: RawBytes,
         value: TokenAmount,
     ) -> SyscallResult<Receipt> {
-        let method = self.method_hasher.method_number(method);
+        let method = self.method_resolver.method_number(method);
         send::send(to, method, params, value)
     }
 }
