a7b39a1f69d52a102a1df5ca3c6f604e675d6459
Alex Su <alexanderytsu@gmail.com>
2022-07-19 16:48:34 +1000

    unixify the method numbering tool

--- fvm_dispatch_tools/src/blake2b.rs
+++ fvm_dispatch_tools/src/blake2b.rs
@@ -1,11 +1,9 @@
+use blake2b_simd::blake2b;
 use fvm_dispatch::hash::Hasher;
-use multihash::Code;
-use multihash::MultihashDigest;
+
 pub struct Blake2bHasher {}
 impl Hasher for Blake2bHasher {
     fn hash(&self, bytes: &[u8]) -> Vec<u8> {
-        let digest = Code::Blake2b256.digest(bytes);
-        // drop the first 4 bytes of the multihash which identify the hash as blake2b256
-        digest.to_bytes()[4..].to_vec()
+        blake2b(bytes).as_bytes().to_vec()
     }
 }
--- fvm_dispatch_tools/src/main.rs
+++ fvm_dispatch_tools/src/main.rs
@@ -1,18 +1,29 @@
 mod blake2b;
 
+use std::io::{self, BufRead};
+use std::process::exit;
+
 use blake2b::Blake2bHasher;
 use clap::Parser;
 use fvm_dispatch::hash::MethodResolver;
+
+const LONG_ABOUT: &str =
+    "Pass a single method name as a command line argument or a list of method names, separated by \
+new-lines to stdin. The output is a list of hashes, one per method name.";
 
 /// Takes a method name and converts it to an FRC-XXX compliant method number
 ///
 /// Can be used by actor authors to precompute the method number for a given exported method to
 /// avoid runtime hasing during dispatch.
 #[derive(Parser, Debug)]
-#[clap(version, about, long_about = None)]
+#[clap(
+    version,
+    about,
+    long_about = Some(LONG_ABOUT)
+)]
 struct Args {
     /// Method name to hash
-    method_name: String,
+    method_name: Option<String>,
 }
 
 fn main() {
@@ -20,10 +31,31 @@ fn main() {
     let resolver = MethodResolver::new(Blake2bHasher {});
     let method_name = args.method_name;
 
-    match resolver.method_number(method_name.as_str()) {
+    if method_name.is_none() {
+        // read from std-in if no name passed in
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
+        let mut line = String::new();
+
+        loop {
+            match handle.read_line(&mut line) {
+                Ok(0) => break,
+                Ok(_) => {
+                    let method_name = line.trim().to_string();
+                    let method_number = resolver.method_number(&method_name).unwrap();
+                    println!("{}", method_number);
+                    line.clear();
+                }
+                Err(err) => panic!("error parsing stdin {:?}", err),
+            }
+        }
+
+        exit(0);
+    }
+
+    match resolver.method_number(method_name.unwrap().as_str()) {
         Ok(method_number) => {
-            println!("Method name   : {}", method_name);
-            println!("Method number : {}", method_number);
+            println!("{}", method_number);
         }
         Err(e) => {
             println!("Error computing method name: {:?}", e)
