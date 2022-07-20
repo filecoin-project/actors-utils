207f6b70d08997f3d0c450445b17e3c79de29b8a
Alex Su <7680266+alexytsu@users.noreply.github.com>
2022-07-20 10:58:45 +1000

    Refactor and rename fil_token state layer (#20)
    * rename fil_token -> fil_fungible_token
    
    * fungible_token: rename some public structs
    
    * panic in load_state if state tree missing
    
    * use a shared-memory blockstore for testing
    
    * streamline balance and allowance increase/decreases into single 'change_by' functions
    
    * replace lt < 0 with is_negative
    
    * cleanup

--- fil_fungible_token/src/token/mod.rs
+++ fil_fungible_token/src/token/mod.rs
@@ -2,6 +2,8 @@ pub mod errors;
 pub mod receiver;
 mod state;
 mod types;
+use std::ops::Neg;
+
 use self::state::TokenState;
 pub use self::types::*;
 
@@ -10,30 +12,33 @@ use anyhow::Ok;
 use anyhow::Result;
 use cid::Cid;
 use fvm_ipld_blockstore::Blockstore as IpldStore;
-use fvm_shared::bigint::Zero;
 use fvm_shared::econ::TokenAmount;
 use fvm_shared::ActorID;
+use num_traits::Signed;
 
 /// Library functions that implement core FRC-??? standards
 ///
 /// Holds injectable services to access/interface with IPLD/FVM layer.
-pub struct TokenHelper<BS>
+pub struct Token<BS>
 where
     BS: IpldStore + Clone,
 {
-    /// Injected blockstore
+    /// Injected blockstore. The blockstore must reference the same underlying storage under Clone
     bs: BS,
     /// Root of the token state tree
-    token_state: Cid,
+    state_cid: Cid,
 }
 
-impl<BS> TokenHelper<BS>
+impl<BS> Token<BS>
 where
     BS: IpldStore + Clone,
 {
     /// Instantiate a token helper with access to a blockstore and runtime
     pub fn new(bs: BS, token_state: Cid) -> Self {
-        Self { bs, token_state }
+        Self {
+            bs,
+            state_cid: token_state,
+        }
     }
 
     /// Constructs the token state tree and saves it at a CID
@@ -43,22 +48,27 @@ where
     }
 
     /// Helper function that loads the root of the state tree related to token-accounting
-    fn load_state(&self) -> Result<TokenState> {
-        TokenState::load(&self.bs, &self.token_state)
+    ///
+    /// Actors can't usefully recover if state wasn't initialized (failure to call `init_state`) in
+    /// the constructor so this method panics if the state tree if missing
+    fn load_state(&self) -> TokenState {
+        TokenState::load(&self.bs, &self.state_cid).unwrap()
     }
 
     /// Mints the specified value of tokens into an account
     ///
-    /// If the total supply or account balance overflows, this method returns an error. The mint
-    /// amount must be non-negative or the method returns an error.
+    /// The mint amount must be non-negative or the method returns an error
     pub fn mint(&self, initial_holder: ActorID, value: TokenAmount) -> Result<()> {
-        if value.lt(&TokenAmount::zero()) {
+        if value.is_negative() {
             bail!("value of mint was negative {}", value);
         }
 
         // Increase the balance of the actor and increase total supply
-        let mut state = self.load_state()?;
-        state.increase_balance(&self.bs, initial_holder, &value)?;
+        let mut state = self.load_state();
+        state.change_balance_by(&self.bs, initial_holder, &value)?;
+
+        // TODO: invoke the receiver hook on the initial_holder
+
         state.increase_supply(&value)?;
 
         // Commit the state atomically if supply and balance increased
@@ -72,7 +82,7 @@ where
     /// This equals the sum of `balance_of` called on all addresses. This equals sum of all
     /// successful `mint` calls minus the sum of all successful `burn`/`burn_from` calls
     pub fn total_supply(&self) -> TokenAmount {
-        let state = self.load_state().unwrap();
+        let state = self.load_state();
         state.supply
     }
 
@@ -81,7 +91,7 @@ where
     /// Accounts that have never received transfers implicitly have a zero-balance
     pub fn balance_of(&self, holder: ActorID) -> Result<TokenAmount> {
         // Load the HAMT holding balances
-        let state = self.load_state()?;
+        let state = self.load_state();
         state.get_balance(&self.bs, holder)
     }
 
@@ -90,14 +100,14 @@ where
     /// The allowance is the amount that the spender can transfer or burn out of the owner's account
     /// via the `transfer_from` and `burn_from` methods.
     pub fn allowance(&self, owner: ActorID, spender: ActorID) -> Result<TokenAmount> {
-        let state = self.load_state()?;
+        let state = self.load_state();
         let allowance = state.get_allowance_between(&self.bs, owner, spender)?;
         Ok(allowance)
     }
 
     /// Increase the allowance that a spender controls of the owner's balance by the requested delta
     ///
-    /// Returns an error if requested delta is negative or there are errors in (de)sereliazation of
+    /// Returns an error if requested delta is negative or there are errors in (de)serialization of
     /// state. Else returns the new allowance.
     pub fn increase_allowance(
         &self,
@@ -105,12 +115,12 @@ where
         spender: ActorID,
         delta: TokenAmount,
     ) -> Result<TokenAmount> {
-        if delta.lt(&TokenAmount::zero()) {
+        if delta.is_negative() {
             bail!("value of delta was negative {}", delta);
         }
 
-        let mut state = self.load_state()?;
-        let new_amount = state.increase_allowance(&self.bs, owner, spender, &delta)?;
+        let mut state = self.load_state();
+        let new_amount = state.change_allowance_by(&self.bs, owner, spender, &delta)?;
         state.save(&self.bs)?;
 
         Ok(new_amount)
@@ -119,21 +129,20 @@ where
     /// Decrease the allowance that a spender controls of the owner's balance by the requested delta
     ///
     /// If the resulting allowance would be negative, the allowance between owner and spender is set
-    /// to zero. If resulting allowance is zero, the entry is removed from the state map. Returns an
-    /// error if either the spender or owner address is unresolvable. Returns an error if requested
-    /// delta is negative. Else returns the new allowance
+    /// to zero. Returns an error if either the spender or owner address is unresolvable. Returns an
+    /// error if requested delta is negative. Else returns the new allowance
     pub fn decrease_allowance(
         &self,
         owner: ActorID,
         spender: ActorID,
         delta: TokenAmount,
     ) -> Result<TokenAmount> {
-        if delta.lt(&TokenAmount::zero()) {
+        if delta.is_negative() {
             bail!("value of delta was negative {}", delta);
         }
 
-        let mut state = self.load_state()?;
-        let new_allowance = state.decrease_allowance(&self.bs, owner, spender, &delta)?;
+        let mut state = self.load_state();
+        let new_allowance = state.change_allowance_by(&self.bs, owner, spender, &delta.neg())?;
         state.save(&self.bs)?;
 
         Ok(new_allowance)
@@ -141,7 +150,7 @@ where
 
     /// Sets the allowance between owner and spender to 0
     pub fn revoke_allowance(&self, owner: ActorID, spender: ActorID) -> Result<()> {
-        let mut state = self.load_state()?;
+        let mut state = self.load_state();
         state.revoke_allowance(&self.bs, owner, spender)?;
         state.save(&self.bs)?;
         Ok(())
@@ -157,37 +166,37 @@ where
     /// - The target's balance MUST decrease by the requested value
     /// - The total_supply MUST decrease by the requested value
     ///
-    /// ## Operator equals target address
-    /// If the operator is the targeted address, they are implicitly approved to burn an unlimited
+    /// ## Spender equals owner address
+    /// If the spender is the targeted address, they are implicitly approved to burn an unlimited
     /// amount of tokens (up to their balance)
     ///
-    /// ## Operator burning on behalf of target address
-    /// If the operator is burning on behalf of the target token holder the following preconditions
+    /// ## Spender burning on behalf of owner address
+    /// If the spender is burning on behalf of the owner the following preconditions
     /// must be met on top of the general burn conditions:
-    /// - The operator MUST have an allowance not less than the requested value
+    /// - The spender MUST have an allowance not less than the requested value
     /// In addition to the general postconditions:
-    /// - The target-operator allowance MUST decrease by the requested value
+    /// - The target-spender allowance MUST decrease by the requested value
     ///
-    /// If the burn operation would result in a negative balance for the targeted address, the burn
-    /// is discarded and this method returns an error
+    /// If the burn operation would result in a negative balance for the owner, the burn is
+    /// discarded and this method returns an error
     pub fn burn(
         &self,
         spender: ActorID,
         owner: ActorID,
         value: TokenAmount,
     ) -> Result<TokenAmount> {
-        if value.lt(&TokenAmount::zero()) {
-            bail!("Cannot burn a negative amount");
+        if value.is_negative() {
+            bail!("cannot burn a negative amount");
         }
 
-        let mut state = self.load_state()?;
+        let mut state = self.load_state();
 
         if spender != owner {
             // attempt to use allowance and return early if not enough
             state.attempt_use_allowance(&self.bs, spender, owner, &value)?;
         }
         // attempt to burn the requested amount
-        let new_amount = state.decrease_balance(&self.bs, owner, &value)?;
+        let new_amount = state.change_balance_by(&self.bs, owner, &value.neg())?;
 
         // if both succeeded, atomically commit the transaction
         state.save(&self.bs)?;
@@ -208,42 +217,43 @@ where
     /// - The senders's balance MUST decrease by the requested value
     /// - The receiver's balance MUST increase by the requested value
     ///
-    /// ## Operator equals target address
-    /// If the operator is the 'from' address, they are implicitly approved to transfer an unlimited
+    /// ## Spender equals owner address
+    /// If the spender is the owner address, they are implicitly approved to transfer an unlimited
     /// amount of tokens (up to their balance)
     ///
-    /// ## Operator transferring on behalf of target address
-    /// If the operator is transferring on behalf of the target token holder the following preconditions
+    /// ## Spender transferring on behalf of owner address
+    /// If the spender is transferring on behalf of the target token holder the following preconditions
     /// must be met on top of the general burn conditions:
-    /// - The operator MUST have an allowance not less than the requested value
+    /// - The spender MUST have an allowance not less than the requested value
     /// In addition to the general postconditions:
-    /// - The from-operator allowance MUST decrease by the requested value
+    /// - The owner-spender allowance MUST decrease by the requested value
     pub fn transfer(
         &self,
-        operator: ActorID,
-        from: ActorID,
-        to: ActorID,
+        spender: ActorID,
+        owner: ActorID,
+        receiver: ActorID,
         value: TokenAmount,
     ) -> Result<()> {
-        if value.lt(&TokenAmount::zero()) {
-            bail!("Cannot transfer a negative amount");
+        if value.is_negative() {
+            bail!("cannot transfer a negative amount");
         }
 
-        let mut state = self.load_state()?;
+        let mut state = self.load_state();
 
-        if operator != from {
+        if spender != owner {
             // attempt to use allowance and return early if not enough
-            state.attempt_use_allowance(&self.bs, operator, from, &value)?;
+            state.attempt_use_allowance(&self.bs, spender, owner, &value)?;
         }
+
+        // attempt to credit the receiver
+        state.change_balance_by(&self.bs, receiver, &value)?;
+        // attempt to debit from the sender
+        state.change_balance_by(&self.bs, owner, &value.neg())?;
 
         // call the receiver hook
         // FIXME: use fvm_dispatch to make a standard runtime call to the receiver
         // - ensure the hook did not abort
-
-        // attempt to debit from the sender
-        state.decrease_balance(&self.bs, from, &value)?;
-        // attempt to credit the receiver
-        state.increase_balance(&self.bs, to, &value)?;
+        // - receiver hook should see the new balances...
 
         // if all succeeded, atomically commit the transaction
         state.save(&self.bs)?;
