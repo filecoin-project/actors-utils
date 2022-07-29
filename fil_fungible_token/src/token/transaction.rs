eaa56226f8ccc88bc912bff7cdd0ac624fc57709
Alex Su <alexanderytsu@gmail.com>
2022-07-29 10:40:30 +1000

    simplify transaction builder API

--- fil_fungible_token/src/token/transaction.rs
+++ fil_fungible_token/src/token/transaction.rs
@@ -25,99 +25,41 @@ pub enum TransactionOutcome {
     Reverted(Cid),
 }
 
-pub struct CleanTransaction<BS, MC>
+pub struct TokenTransaction<BS, MC>
 where
     BS: IpldStore + Clone,
     MC: MethodCaller,
 {
     token: Rc<RefCell<Token<BS, MC>>>,
     needs_rollback: bool,
+    state_dirty: bool,
 }
 
-pub struct DirtyTransaction<BS, MC>
+// impl<BS, MC> Clone for TokenTransaction<BS, MC>
+// where
+//     BS: IpldStore + Clone,
+//     MC: MethodCaller,
+// {
+//     fn clone(&self) -> Self {
+//         TokenTransaction { token: self.token.clone(), needs_rollback: self.needs_rollback }
+//     }
+// }
+
+impl<BS, MC> TokenTransaction<BS, MC>
 where
     BS: IpldStore + Clone,
     MC: MethodCaller,
 {
-    token: Rc<RefCell<Token<BS, MC>>>,
-    needs_rollback: bool,
-}
-
-impl<BS, MC> Clone for DirtyTransaction<BS, MC>
-where
-    BS: IpldStore + Clone,
-    MC: MethodCaller,
-{
-    fn clone(&self) -> Self {
-        DirtyTransaction { token: self.token.clone(), needs_rollback: self.needs_rollback }
+    pub fn new(token: Token<BS, MC>) -> Self {
+        Self { needs_rollback: false, state_dirty: false, token: Rc::new(RefCell::new(token)) }
     }
-}
 
-impl<BS, MC> From<CleanTransaction<BS, MC>> for DirtyTransaction<BS, MC>
-where
-    BS: IpldStore + Clone,
-    MC: MethodCaller,
-{
-    fn from(tx: CleanTransaction<BS, MC>) -> Self {
-        Self { token: tx.token, needs_rollback: tx.needs_rollback }
-    }
-}
-
-impl<BS, MC> From<&mut CleanTransaction<BS, MC>> for DirtyTransaction<BS, MC>
-where
-    BS: IpldStore + Clone,
-    MC: MethodCaller,
-{
-    fn from(tx: &mut CleanTransaction<BS, MC>) -> Self {
-        Self { token: tx.token.clone(), needs_rollback: tx.needs_rollback }
-    }
-}
-
-pub trait StateModifier<BS, MC>
-where
-    BS: IpldStore + Clone,
-    MC: MethodCaller,
-{
-    fn increase_allowance(
-        &mut self,
-        owner: ActorID,
-        spender: ActorID,
-        delta: &TokenAmount,
-    ) -> DirtyTransaction<BS, MC>;
-
-    fn decrease_allowance(
-        &mut self,
-        owner: ActorID,
-        spender: ActorID,
-        delta: &TokenAmount,
-    ) -> DirtyTransaction<BS, MC>;
-
-    fn revoke_allowance(&mut self, owner: ActorID, spender: ActorID) -> DirtyTransaction<BS, MC>;
-
-    fn burn(
-        &mut self,
-        spender: ActorID,
-        owner: ActorID,
-        amount: &TokenAmount,
-    ) -> DirtyTransaction<BS, MC>;
-}
-
-pub trait Transaction<BS, MC>
-where
-    BS: IpldStore + Clone,
-    MC: MethodCaller,
-{
-    fn flush(&mut self) -> Result<TransactionOutcome>;
-}
-
-impl<BS, MC> CleanTransaction<BS, MC>
-where
-    BS: IpldStore + Clone,
-    MC: MethodCaller,
-{
-    pub fn new(bs: BS, mc: MC, token_cid: Cid) -> Result<Self> {
-        let token = Token::load(bs, mc, token_cid)?;
-        Ok(Self { token: Rc::new(RefCell::new(token)), needs_rollback: false })
+    pub fn flush(&mut self) -> Result<TransactionOutcome> {
+        if self.state_dirty && self.needs_rollback {
+            Ok(TransactionOutcome::Reverted(self.token.borrow_mut().revert()?))
+        } else {
+            Ok(TransactionOutcome::Succeeded(self.token.borrow_mut().flush()?))
+        }
     }
 
     fn apply_state_change<F, Res>(&mut self, f: F) -> &mut Self
@@ -135,207 +77,142 @@ where
 
         self
     }
+}
 
-    pub fn mint_and_flush(
+impl<BS, MC> TokenTransaction<BS, MC>
+where
+    BS: IpldStore + Clone,
+    MC: MethodCaller,
+{
+    pub fn increase_allowance(
+        &mut self,
+        owner: ActorID,
+        spender: ActorID,
+        delta: &TokenAmount,
+    ) -> &mut TokenTransaction<BS, MC> {
+        self.state_dirty = true;
+        self.apply_state_change(|mut token| token.increase_allowance(owner, spender, delta))
+    }
+
+    pub fn decrease_allowance(
+        &mut self,
+        owner: ActorID,
+        spender: ActorID,
+        delta: &TokenAmount,
+    ) -> &mut TokenTransaction<BS, MC> {
+        self.state_dirty = true;
+        self.apply_state_change(|mut token| token.decrease_allowance(owner, spender, delta))
+    }
+
+    pub fn revoke_allowance(
+        &mut self,
+        owner: ActorID,
+        spender: ActorID,
+    ) -> &mut TokenTransaction<BS, MC> {
+        self.state_dirty = true;
+        self.apply_state_change(|mut token| token.revoke_allowance(owner, spender))
+    }
+
+    pub fn burn(
+        &mut self,
+        spender: ActorID,
+        owner: ActorID,
+        amount: &TokenAmount,
+    ) -> &mut TokenTransaction<BS, MC> {
+        self.state_dirty = true;
+        self.apply_state_change(|mut token| token.burn(spender, owner, amount))
+    }
+
+    pub fn mint(
         &mut self,
         minter: ActorID,
         initial_holder: ActorID,
         value: &TokenAmount,
         data: &[u8],
-    ) -> &mut CleanTransaction<BS, MC> {
+    ) -> &mut TokenTransaction<BS, MC> {
+        if self.state_dirty {
+            self.needs_rollback = true;
+            return self;
+        }
         self.apply_state_change(|mut token| token.mint(minter, initial_holder, value, data))
     }
 
-    pub fn transfer_and_flush(
+    pub fn transfer(
         &mut self,
         spender: ActorID,
         owner: ActorID,
         receiver: ActorID,
         value: &TokenAmount,
         data: &[u8],
-    ) -> &mut CleanTransaction<BS, MC> {
+    ) -> &mut TokenTransaction<BS, MC> {
+        if self.state_dirty {
+            self.needs_rollback = true;
+            return self;
+        }
         self.apply_state_change(|mut token| token.transfer(spender, owner, receiver, value, data))
     }
 }
 
-impl<BS, MC> StateModifier<BS, MC> for CleanTransaction<BS, MC>
-where
-    BS: IpldStore + Clone,
-    MC: MethodCaller,
-{
-    fn increase_allowance(
-        &mut self,
-        owner: ActorID,
-        spender: ActorID,
-        delta: &TokenAmount,
-    ) -> DirtyTransaction<BS, MC> {
-        self.apply_state_change(|mut token| token.increase_allowance(owner, spender, delta));
-        self.into()
-    }
+// #[cfg(test)]
+// mod test {
+//     use fvm_shared::{econ::TokenAmount, ActorID};
+//     use num_traits::Zero;
 
-    fn decrease_allowance(
-        &mut self,
-        owner: ActorID,
-        spender: ActorID,
-        delta: &TokenAmount,
-    ) -> DirtyTransaction<BS, MC> {
-        self.apply_state_change(|mut token| token.decrease_allowance(owner, spender, delta));
-        self.into()
-    }
+//     const TOKEN_ACTOR_ADDRESS: ActorID = ActorID::max_value();
+//     const TREASURY: ActorID = 1;
+//     // const ALICE: ActorID = 2;
+//     // const BOB: ActorID = 3;
 
-    fn revoke_allowance(&mut self, owner: ActorID, spender: ActorID) -> DirtyTransaction<BS, MC> {
-        self.apply_state_change(|mut token| token.revoke_allowance(owner, spender));
-        self.into()
-    }
+//     use crate::{
+//         blockstore::SharedMemoryBlockstore,
+//         method::FakeMethodCaller,
+//         token::{
+//             transaction::{StateModifier, Transaction},
+//             Token,
+//         },
+//     };
 
-    fn burn(
-        &mut self,
-        spender: ActorID,
-        owner: ActorID,
-        amount: &TokenAmount,
-    ) -> DirtyTransaction<BS, MC> {
-        self.apply_state_change(|mut token| token.burn(spender, owner, amount));
-        self.into()
-    }
-}
+//     use super::TransactionOutcome;
 
-impl<BS, MC> DirtyTransaction<BS, MC>
-where
-    BS: IpldStore + Clone,
-    MC: MethodCaller,
-{
-    fn apply_state_change<F, Res>(&mut self, f: F) -> &mut Self
-    where
-        F: FnOnce(RefMut<Token<BS, MC>>) -> TokenResult<Res>,
-    {
-        if self.needs_rollback {
-            return self;
-        }
+//     fn new_transaction() -> CleanTransaction<SharedMemoryBlockstore, FakeMethodCaller> {
+//         let bs = SharedMemoryBlockstore::new();
+//         let (_token, cid) = Token::new(bs.clone(), FakeMethodCaller::default()).unwrap();
 
-        if let Err(_e) = f((*self.token).borrow_mut()) {
-            // TODO: error logging, tracking or propagation of error up?
-            self.needs_rollback = true;
-        }
+//         CleanTransaction::new(bs, FakeMethodCaller::default(), cid).unwrap()
+//     }
 
-        self
-    }
-}
+//     #[test]
+//     fn it_batches_changes() {
+//         let mut tx = new_transaction();
 
-impl<BS, MC> StateModifier<BS, MC> for DirtyTransaction<BS, MC>
-where
-    BS: IpldStore + Clone,
-    MC: MethodCaller,
-{
-    fn increase_allowance(
-        &mut self,
-        owner: ActorID,
-        spender: ActorID,
-        delta: &TokenAmount,
-    ) -> DirtyTransaction<BS, MC> {
-        self.apply_state_change(|mut token| token.increase_allowance(owner, spender, delta));
-        self.clone()
-    }
+//         let res = tx
+//             .mint_and_flush(TOKEN_ACTOR_ADDRESS, TREASURY, &TokenAmount::from(100), &[])
+//             .burn(TREASURY, TREASURY, &TokenAmount::from(60))
+//             .flush()
+//             .unwrap();
 
-    fn decrease_allowance(
-        &mut self,
-        owner: ActorID,
-        spender: ActorID,
-        delta: &TokenAmount,
-    ) -> DirtyTransaction<BS, MC> {
-        self.apply_state_change(|mut token| token.decrease_allowance(owner, spender, delta));
-        self.clone()
-    }
+//         if let TransactionOutcome::Succeeded(_) = res {
+//             assert_eq!(tx.token.borrow().balance_of(TREASURY).unwrap(), TokenAmount::from(40));
+//         } else {
+//             panic!("expected success");
+//         }
+//     }
 
-    fn revoke_allowance(&mut self, owner: ActorID, spender: ActorID) -> DirtyTransaction<BS, MC> {
-        self.apply_state_change(|mut token| token.revoke_allowance(owner, spender));
-        self.clone()
-    }
+//     #[test]
+//     fn it_fails_atomically() {
+//         let mut tx = new_transaction();
 
-    fn burn(
-        &mut self,
-        spender: ActorID,
-        owner: ActorID,
-        amount: &TokenAmount,
-    ) -> DirtyTransaction<BS, MC> {
-        self.apply_state_change(|mut token| token.burn(spender, owner, amount));
-        self.clone()
-    }
-}
+//         // burn more than was minted
+//         let res = tx
+//             .mint_and_flush(TOKEN_ACTOR_ADDRESS, TREASURY, &TokenAmount::from(100), &[])
+//             .burn(TREASURY, TREASURY, &TokenAmount::from(110))
+//             .flush()
+//             .unwrap();
 
-impl<BS, MC> Transaction<BS, MC> for DirtyTransaction<BS, MC>
-where
-    BS: IpldStore + Clone,
-    MC: MethodCaller,
-{
-    fn flush(&mut self) -> Result<TransactionOutcome> {
-        if self.needs_rollback {
-            Ok(TransactionOutcome::Reverted(self.token.borrow_mut().revert()?))
-        } else {
-            Ok(TransactionOutcome::Succeeded(self.token.borrow_mut().flush()?))
-        }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use fvm_shared::{econ::TokenAmount, ActorID};
-    use num_traits::Zero;
-
-    const TOKEN_ACTOR_ADDRESS: ActorID = ActorID::max_value();
-    const TREASURY: ActorID = 1;
-    // const ALICE: ActorID = 2;
-    // const BOB: ActorID = 3;
-
-    use crate::{
-        blockstore::SharedMemoryBlockstore,
-        method::FakeMethodCaller,
-        token::{
-            transaction::{StateModifier, Transaction},
-            Token,
-        },
-    };
-
-    use super::{CleanTransaction, TransactionOutcome};
-
-    fn new_transaction() -> CleanTransaction<SharedMemoryBlockstore, FakeMethodCaller> {
-        let bs = SharedMemoryBlockstore::new();
-        let (_token, cid) = Token::new(bs.clone(), FakeMethodCaller::default()).unwrap();
-
-        CleanTransaction::new(bs, FakeMethodCaller::default(), cid).unwrap()
-    }
-
-    #[test]
-    fn it_batches_changes() {
-        let mut tx = new_transaction();
-
-        let res = tx
-            .mint_and_flush(TOKEN_ACTOR_ADDRESS, TREASURY, &TokenAmount::from(100), &[])
-            .burn(TREASURY, TREASURY, &TokenAmount::from(60))
-            .flush()
-            .unwrap();
-
-        if let TransactionOutcome::Succeeded(_) = res {
-            assert_eq!(tx.token.borrow().balance_of(TREASURY).unwrap(), TokenAmount::from(40));
-        } else {
-            panic!("expected success");
-        }
-    }
-
-    #[test]
-    fn it_fails_atomically() {
-        let mut tx = new_transaction();
-
-        // burn more than was minted
-        let res = tx
-            .mint_and_flush(TOKEN_ACTOR_ADDRESS, TREASURY, &TokenAmount::from(100), &[])
-            .burn(TREASURY, TREASURY, &TokenAmount::from(110))
-            .flush()
-            .unwrap();
-
-        if let TransactionOutcome::Reverted(_) = res {
-            assert_eq!(tx.token.borrow().balance_of(TREASURY).unwrap(), TokenAmount::zero());
-        } else {
-            panic!("expected revert");
-        }
-    }
-}
+//         if let TransactionOutcome::Reverted(_) = res {
+//             assert_eq!(tx.token.borrow().balance_of(TREASURY).unwrap(), TokenAmount::zero());
+//         } else {
+//             panic!("expected revert");
+//         }
+//     }
+// }
