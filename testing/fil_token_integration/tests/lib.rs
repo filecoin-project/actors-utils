0298824ddd959dff3d0d98e7769ead3f45ea34db
Andrew Bright <abright@users.noreply.github.com>
2022-08-17 11:32:12 +1000

    use a helper function to simplify messaging in tests

--- testing/fil_token_integration/tests/lib.rs
+++ testing/fil_token_integration/tests/lib.rs
@@ -2,6 +2,7 @@ use std::env;
 
 use fil_fungible_token::token::state::TokenState;
 use fvm::executor::{ApplyKind, Executor};
+use fvm_dispatch::method_hash;
 use fvm_integration_tests::tester::{Account, Tester};
 use fvm_ipld_blockstore::MemoryBlockstore;
 use fvm_ipld_encoding::RawBytes;
@@ -27,7 +28,7 @@ fn mint_tokens() {
     // Get wasm bin
     let wasm_path =
         env::current_dir().unwrap().join(BASIC_TOKEN_ACTOR_WASM).canonicalize().unwrap();
-    let wasm_bin = std::fs::read(wasm_path).expect("Unable to read file");
+    let wasm_bin = std::fs::read(wasm_path).expect("Unable to read token actor file");
 
     // Set actor state
     let actor_state = TokenState::new(&blockstore).unwrap(); // TODO: this should probably not be exported from the package
@@ -39,61 +40,38 @@ fn mint_tokens() {
     // Instantiate machine
     tester.instantiate_machine().unwrap();
 
-    let message = Message {
-        from: minter[0].1,
-        to: actor_address,
-        gas_limit: 99999999,
-        method_num: 1, // 1 is constructor
-        sequence: 0,
-        ..Message::default()
+    // Helper to simplify sending messages
+    let mut sequence = 0u64;
+    let mut call_method = |from, to, method_num, params| {
+        let message = Message {
+            from,
+            to,
+            gas_limit: 99999999,
+            method_num,
+            sequence,
+            params: if let Some(params) = params { params } else { RawBytes::default() },
+            ..Message::default()
+        };
+        sequence += 1;
+        tester
+            .executor
+            .as_mut()
+            .unwrap()
+            .execute_message(message, ApplyKind::Explicit, 100)
+            .unwrap()
     };
 
-    let ret_val = tester
-        .executor
-        .as_mut()
-        .unwrap()
-        .execute_message(message, ApplyKind::Explicit, 100)
-        .unwrap();
+    // Construct the token actor
+    let ret_val = call_method(minter[0].1, actor_address, method_hash!("Constructor"), None);
+    println!("token actor constructor return data: {:#?}", &ret_val);
 
-    println!("constructor return data {:#?}", &ret_val);
-
-    let message = Message {
-        from: minter[0].1,
-        to: actor_address,
-        gas_limit: 99999999,
-        method_num: 3839021839, // mint
-        sequence: 1,
-        ..Message::default()
-    };
-
-    let ret_val = tester
-        .executor
-        .as_mut()
-        .unwrap()
-        .execute_message(message, ApplyKind::Explicit, 100)
-        .unwrap();
-
+    // Mint some tokens
+    let ret_val = call_method(minter[0].1, actor_address, method_hash!("Mint"), None);
     println!("mint return data {:#?}", &ret_val);
 
+    // Check balance
     let params = RawBytes::serialize(minter[0].1).unwrap();
-
-    let message = Message {
-        from: minter[0].1,
-        to: actor_address,
-        gas_limit: 99999999,
-        method_num: 1568445334, // 5 is balance of
-        sequence: 2,
-        params,
-        ..Message::default()
-    };
-
-    let ret_val = tester
-        .executor
-        .as_mut()
-        .unwrap()
-        .execute_message(message, ApplyKind::Explicit, 100)
-        .unwrap();
-
+    let ret_val = call_method(minter[0].1, actor_address, method_hash!("BalanceOf"), Some(params));
     println!("balance return data {:#?}", &ret_val);
 
     let return_data = ret_val.msg_receipt.return_data;
