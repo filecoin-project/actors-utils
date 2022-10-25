ec249ed04a6fa89e8bd4492e3a382e584d9668df
Andrew Bright <abright@users.noreply.github.com>
2022-10-25 15:14:11 +1100

    Store TokenState directly in BasicToken
    instead of storing the Token handle, we're storing the actual state now so it can be easily stored alongside some other token data

--- token_examples/include_token/src/token.rs
+++ token_examples/include_token/src/token.rs
@@ -1,6 +1,7 @@
-use cid::Cid;
+use cid::{multihash::Code, Cid};
 use frc42_dispatch::match_method;
 use frc46_token::token::{
+    state::{StateError, TokenState},
     types::{
         AllowanceReturn, BalanceReturn, BurnFromReturn, BurnParams, BurnReturn,
         DecreaseAllowanceParams, FRC46Token, GetAllowanceParams, GranularityReturn,
@@ -10,14 +11,17 @@ use frc46_token::token::{
     Token, TokenError,
 };
 use fvm_actor_utils::{
-    blockstore::Blockstore, messaging::FvmMessenger, receiver::ReceiverHookError,
+    blockstore::Blockstore,
+    messaging::{FvmMessenger, Messaging, MessagingError},
+    receiver::ReceiverHookError,
 };
+use fvm_ipld_blockstore::{Block, Blockstore as _BS};
 use fvm_ipld_encoding::{
     tuple::{Deserialize_tuple, Serialize_tuple},
-    Cbor, RawBytes, DAG_CBOR,
+    Cbor, CborStore, RawBytes, DAG_CBOR,
 };
 use fvm_sdk::{self as sdk, error::NoStateError, sys::ErrorNumber, NO_DATA_BLOCK_ID};
-use fvm_shared::{address::Address, econ::TokenAmount, error::ExitCode};
+use fvm_shared::{address::Address, bigint::Zero, econ::TokenAmount};
 use serde::{de::DeserializeOwned, ser::Serialize};
 use thiserror::Error;
 
@@ -37,6 +41,16 @@ pub enum RuntimeError {
     Blockstore(#[from] ErrorNumber),
     #[error("actor state not found {0}")]
     NoState(#[from] NoStateError),
+    // deserialisation error when loading state
+    #[error("error loading state {0}")]
+    Deserialization(String),
+    // serialisation error when saving state
+    #[error("error saving state {0}")]
+    Serialization(String),
+    #[error("underlying state error {0}")]
+    State(#[from] StateError),
+    #[error("actor messaging error {0}")]
+    Messaging(#[from] MessagingError),
 }
 
 pub fn caller_address() -> Address {
@@ -50,10 +64,10 @@ pub fn caller_address() -> Address {
 
 // this token implementation is designed to be embedded into some larger actor
 // where the state contains more than just the token
-
-pub struct BasicToken<'state> {
+#[derive(Serialize_tuple, Deserialize_tuple, Debug)]
+pub struct BasicToken {
     /// Default token helper impl
-    pub util: Token<'state, Blockstore, FvmMessenger>,
+    pub token: TokenState,
     /// basic token identifier stuff, should it go here or store separately alongside the state
     pub name: String,
     pub symbol: String,
@@ -63,7 +77,7 @@ pub struct BasicToken<'state> {
 /// Implementation of the token API in a FVM actor
 ///
 /// Here the Ipld parameter structs are marshalled and passed to the underlying library functions
-impl FRC46Token<RuntimeError> for BasicToken<'_> {
+impl FRC46Token<RuntimeError> for BasicToken {
     fn name(&self) -> String {
         //String::from("FRC-0046 Token")
         self.name.clone()
@@ -80,16 +94,31 @@ impl FRC46Token<RuntimeError> for BasicToken<'_> {
     }
 
     fn total_supply(&self) -> TotalSupplyReturn {
-        self.util.total_supply()
+        // TODO: token() wants mutable ref, we don't have one here and can't change the interface
+        // so need an immutable wrapper to call these things?
+        // or just bypass it and go to the state directly
+        //self.token().total_supply()
+        self.token.supply.clone()
     }
 
     fn balance_of(&self, params: Address) -> Result<BalanceReturn, RuntimeError> {
-        Ok(self.util.balance_of(&params)?)
+        // TODO: same situation as total_supply
+        //Ok(self.token().balance_of(&params)?)
+        let bs = Blockstore::default();
+        let msg = FvmMessenger::default();
+        match msg.resolve_id(&params) {
+            Ok(owner) => Ok(self.token.get_balance(&bs, owner)?),
+            Err(MessagingError::AddressNotResolved(_)) => {
+                // uninitialized address has implicit zero balance
+                Ok(TokenAmount::zero())
+            }
+            Err(e) => Err(e.into()),
+        }
     }
 
     fn transfer(&mut self, params: TransferParams) -> Result<TransferReturn, RuntimeError> {
         let operator = caller_address();
-        let mut hook = self.util.transfer(
+        let mut hook = self.token().transfer(
             &operator,
             &params.to,
             &params.amount,
@@ -97,13 +126,13 @@ impl FRC46Token<RuntimeError> for BasicToken<'_> {
             RawBytes::default(),
         )?;
 
-        let cid = self.util.flush()?;
+        let cid = self.token().flush()?;
         sdk::sself::set_root(&cid).unwrap();
 
-        let hook_ret = hook.call(self.util.msg())?;
+        let hook_ret = hook.call(self.token().msg())?;
 
         self.reload(&cid)?;
-        let ret = self.util.transfer_return(hook_ret)?;
+        let ret = self.token().transfer_return(hook_ret)?;
 
         Ok(ret)
     }
@@ -113,7 +142,7 @@ impl FRC46Token<RuntimeError> for BasicToken<'_> {
         params: TransferFromParams,
     ) -> Result<TransferFromReturn, RuntimeError> {
         let operator = caller_address();
-        let mut hook = self.util.transfer_from(
+        let mut hook = self.token().transfer_from(
             &operator,
             &params.from,
             &params.to,
@@ -122,13 +151,13 @@ impl FRC46Token<RuntimeError> for BasicToken<'_> {
             RawBytes::default(),
         )?;
 
-        let cid = self.util.flush()?;
+        let cid = self.token().flush()?;
         sdk::sself::set_root(&cid).unwrap();
 
-        let hook_ret = hook.call(self.util.msg())?;
+        let hook_ret = hook.call(self.token().msg())?;
 
         self.reload(&cid)?;
-        let ret = self.util.transfer_from_return(hook_ret)?;
+        let ret = self.token().transfer_from_return(hook_ret)?;
 
         Ok(ret)
     }
@@ -139,7 +168,7 @@ impl FRC46Token<RuntimeError> for BasicToken<'_> {
     ) -> Result<AllowanceReturn, RuntimeError> {
         let owner = caller_address();
         let new_allowance =
-            self.util.increase_allowance(&owner, &params.operator, &params.increase)?;
+            self.token().increase_allowance(&owner, &params.operator, &params.increase)?;
         Ok(new_allowance)
     }
 
@@ -149,24 +178,24 @@ impl FRC46Token<RuntimeError> for BasicToken<'_> {
     ) -> Result<AllowanceReturn, RuntimeError> {
         let owner = caller_address();
         let new_allowance =
-            self.util.decrease_allowance(&owner, &params.operator, &params.decrease)?;
+            self.token().decrease_allowance(&owner, &params.operator, &params.decrease)?;
         Ok(new_allowance)
     }
 
     fn revoke_allowance(&mut self, params: RevokeAllowanceParams) -> Result<(), RuntimeError> {
         let owner = caller_address();
-        self.util.revoke_allowance(&owner, &params.operator)?;
+        self.token().revoke_allowance(&owner, &params.operator)?;
         Ok(())
     }
 
     fn allowance(&mut self, params: GetAllowanceParams) -> Result<AllowanceReturn, RuntimeError> {
-        let allowance = self.util.allowance(&params.owner, &params.operator)?;
+        let allowance = self.token().allowance(&params.owner, &params.operator)?;
         Ok(allowance)
     }
 
     fn burn(&mut self, params: BurnParams) -> Result<BurnReturn, RuntimeError> {
         let caller = caller_address();
-        let res = self.util.burn(&caller, &params.amount)?;
+        let res = self.token().burn(&caller, &params.amount)?;
         Ok(res)
     }
 
@@ -175,7 +204,7 @@ impl FRC46Token<RuntimeError> for BasicToken<'_> {
         params: frc46_token::token::types::BurnFromParams,
     ) -> Result<BurnFromReturn, RuntimeError> {
         let caller = caller_address();
-        let res = self.util.burn_from(&caller, &params.owner, &params.amount)?;
+        let res = self.token().burn_from(&caller, &params.owner, &params.amount)?;
         Ok(res)
     }
 }
@@ -194,18 +223,46 @@ impl Cbor for MintParams {}
 // if BasicToken is a user implementation here then i don't need to worry about it so much
 // i should move the load/save stuff here though, then FRC46Token methods can call into it
 // it won't bother the invoke helper though, all it does is translate the incoming method number+params to token interface calls
-impl BasicToken<'_> {
+impl BasicToken {
+    pub fn new(name: String, symbol: String, granularity: u64) -> Self {
+        let bs = Blockstore::default();
+        BasicToken { token: TokenState::new(&bs).unwrap(), name, symbol, granularity }
+    }
+
+    pub fn token(&mut self) -> Token<'_, Blockstore, FvmMessenger> {
+        let bs = Blockstore::default();
+        let msg = FvmMessenger::default();
+        Token::wrap(bs, msg, self.granularity, &mut self.token)
+    }
+
+    pub fn load(cid: &Cid) -> Result<Self, RuntimeError> {
+        let bs = Blockstore::default();
+        match bs.get_cbor::<Self>(cid) {
+            Ok(Some(s)) => Ok(s),
+            // TODO: improve on these errors?
+            Ok(None) => Err(RuntimeError::Deserialization("no data found".into())),
+            Err(e) => Err(RuntimeError::Deserialization(e.to_string())),
+        }
+    }
+
+    pub fn save(&self) -> Result<Cid, RuntimeError> {
+        let bs = Blockstore::default();
+        let serialized = fvm_ipld_encoding::to_vec(self)
+            .map_err(|err| RuntimeError::Serialization(err.to_string()))?;
+        let block = Block { codec: DAG_CBOR, data: serialized };
+        bs.put(Code::Blake2b256, &block).map_err(|err| RuntimeError::Serialization(err.to_string()))
+    }
+
     fn reload(&mut self, initial_cid: &Cid) -> Result<(), RuntimeError> {
-        // todo: revise error type here so it plays nice with the result and doesn't need unwrap
-        let new_cid = sdk::sself::root().unwrap();
+        let new_cid = sdk::sself::root()?;
         if new_cid != *initial_cid {
-            self.util.load_replace(&new_cid)?;
+            self.token().load_replace(&new_cid)?;
         }
         Ok(())
     }
 
-    fn mint(&mut self, params: MintParams) -> Result<MintReturn, RuntimeError> {
-        let mut hook = self.util.mint(
+    pub fn mint(&mut self, params: MintParams) -> Result<MintReturn, RuntimeError> {
+        let mut hook = self.token().mint(
             &caller_address(),
             &params.initial_owner,
             &params.amount,
@@ -213,13 +270,13 @@ impl BasicToken<'_> {
             Default::default(),
         )?;
 
-        let cid = self.util.flush()?;
+        let cid = self.token().flush()?;
         sdk::sself::set_root(&cid).unwrap();
 
-        let hook_ret = hook.call(self.util.msg())?;
+        let hook_ret = hook.call(self.token().msg())?;
 
         self.reload(&cid)?;
-        let ret = self.util.mint_return(hook_ret)?;
+        let ret = self.token().mint_return(hook_ret)?;
 
         Ok(ret)
     }
@@ -231,7 +288,7 @@ pub fn deserialize_params<O: DeserializeOwned>(params: u32) -> O {
     params.deserialize().unwrap()
 }
 
-fn return_ipld<T>(value: &T) -> std::result::Result<u32, RuntimeError>
+pub fn return_ipld<T>(value: &T) -> std::result::Result<u32, RuntimeError>
 where
     T: Serialize + ?Sized,
 {
