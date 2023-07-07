c1b1183599223f90b93eb4acf2533d5d3f8209fc
Alex Su <7680266+alexytsu@users.noreply.github.com>
2023-07-07 16:50:17 +1000

    Bump version numbers for release (#216)
    * chore: update fvm/ipld crates
    
    I'm also moving all the version specifications to the workspace to
    ensure we always use the same versions and to make future updates
    easier.
    
    * bump version numbers
    
    * fix deps for factory token
    
    * patch to a temp bactors branch
    
    * appease cargo
    
    ---------
    
    Co-authored-by: Steven Allen <steven@stebalien.com>

--- testing/integration/tests/common/mod.rs
+++ testing/integration/tests/common/mod.rs
@@ -60,7 +60,7 @@ pub fn load_actor_wasm(path: &str) -> Vec<u8> {
 /// Construct a Tester with the provided blockstore
 /// mainly cuts down on noise with importing the built-in actor bundle and network/state tree versions
 pub fn construct_tester<BS: Blockstore + Clone, E: Externs>(blockstore: &BS) -> Tester<BS, E> {
-    let bundle_root = bundle::import_bundle(&blockstore, actors_v10::BUNDLE_CAR).unwrap();
+    let bundle_root = bundle::import_bundle(&blockstore, actors_v12::BUNDLE_CAR).unwrap();
 
     Tester::new(NetworkVersion::V18, StateTreeVersion::V5, bundle_root, blockstore.clone()).unwrap()
 }
